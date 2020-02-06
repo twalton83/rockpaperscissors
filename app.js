6c4ba9aa57aa244eeab4d29e574a304681ed0cc9
Tatiana <tatianaw83@gmail.com>
2020-02-05 21:05:20 -0800

    Updated view.

--- app.js
+++ app.js
@@ -8,37 +8,37 @@ let playerSelection = null;
 let computerSelection = null;
 
 rockButton.addEventListener("click", function(){
-    playerSelection = "rock"
+    playerSelection = "Rock"
     computerChoice()
     gamePlay(playerSelection, computerSelection)
     
 })
 
 paperButton.addEventListener("click", function(){
-    playerSelection = "paper"
+    playerSelection = "Paper"
     computerChoice()
     gamePlay(playerSelection, computerSelection)
 })
 
 scissorsButton.addEventListener("click", function(){
-    playerSelection = "scissors"
+    playerSelection = "Scissors"
     computerChoice()
     gamePlay(playerSelection, computerSelection)
 })
 
-let selections = ["rock", "paper", "scissors"]
+let selections = ["Rock", "Paper", "Scissors"]
 let winningPairs= [
     {
-        winner: "rock",
-        loser: "scissors"
+        winner: "Rock",
+        loser: "Scissors"
     },
     {
-        winner: "paper",
-        loser: "rock"
+        winner: "Paper",
+        loser: "Rock"
     },
     {
-        winner: "scissors",
-        loser: "paper"
+        winner: "Scissors",
+        loser: "Paper"
     }
 ]
   
@@ -50,17 +50,17 @@ function computerChoice(){
 function gamePlay(player, computer){
     for (let pair of winningPairs) {
         if ((player === pair.winner) && (computer === pair.loser)) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+            document.getElementById("gameDisplay").textContent = `You win! ${playerSelection} beats ${computerSelection}.`
             playerWins++
-            document.getElementById("playerWins").innerHTML = playerWins;
+            document.getElementById("playerWins").textContent = playerWins;
             break
         } else if (player === computer){
-            console.log(`It's a tie! ${computerSelection} and ${playerSelection}`)
+            document.getElementById("gameDisplay").textContent = `It's a tie! You both chose ${playerSelection}.`
             tieGames++ 
             document.getElementById("ties").textContent = tieGames;
             break
         } else if ((player === pair.loser) && (computer === pair.winner)) {
-            console.log(`You lost! ${computerSelection} beats ${playerSelection}`)
+            document.getElementById("gameDisplay").textContent = `You lost! ${computerSelection} beats ${playerSelection}.`;
             computerWins++
             document.getElementById("computerWins").textContent = computerWins;
             break
